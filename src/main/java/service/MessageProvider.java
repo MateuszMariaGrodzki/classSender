4ea8c51fa1f8cd7f4a311cea27d6accd9cc4d7fd
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-04-01 17:24:27 +0200

    added method getMessageByPackage

--- src/main/java/service/MessageProvider.java
+++ src/main/java/service/MessageProvider.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 public class MessageProvider {
 
@@ -21,6 +22,16 @@ public class MessageProvider {
             ex.printStackTrace();
         }
         message += "</pre>";
+        return message;
+    }
+
+    public static String getMessageByPackage(String pathToPackage, String packageName) throws Exception{
+        List<String> classNames = ClassListProvider.getClassNamesByPackageName(
+                ClassListProvider.class.getClassLoader(),packageName);
+        String message = "<h1>W pakiecie " + packageName + " znajdują się " + classNames.size() + "klasy</h1><br>";
+        for(String string : classNames){
+            message += getMessage(pathToPackage + packageName + string + ".java");
+        }
         return message;
     }
 }
